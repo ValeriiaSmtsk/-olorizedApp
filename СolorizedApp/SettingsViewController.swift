2e0f94b60b813538ba4611884c678c867bac49ce
Salnik Valeriia <melfed.salnik@gmail.com>
2022-03-20 00:11:18 +0300

    Add View Controller

---
+++ СolorizedApp/SettingsViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  СolorizedApp
+//
+//  Created by Валерия Сальник on 05.03.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var resultColorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultColorView.layer.cornerRadius = 20
+        
+        setColor()
+        setValue(for: redValueLabel, greenValueLabel, blueValueLabel)
+    }
+
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redValueLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenValueLabel.text = string(from: greenSlider)
+        default:
+            blueValueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    private func setColor() {
+        resultColorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redValueLabel:
+                redValueLabel.text = string(from: redSlider)
+            case greenValueLabel:
+                greenValueLabel.text = string(from: greenSlider)
+            default:
+                blueValueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
+
