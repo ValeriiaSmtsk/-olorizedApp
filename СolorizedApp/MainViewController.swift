9d5d5764d8cc44beec04c3bf39f5ebc119445d17
Salnik Valeriia <melfed.salnik@gmail.com>
2022-03-20 03:39:42 +0300

    Work with logic (data transfer between VC)

--- СolorizedApp/MainViewController.swift
+++ СolorizedApp/MainViewController.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+protocol SettingsViewControllerDelegate {
+    func setColor(color: UIColor)
 }
+
+class MainViewController: UIViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.delegate = self
+        settingsVC.backgroundColor = view.backgroundColor
+    }
+}
+
+// MARK: - SettingsViewControllerDelegate
+extension MainViewController: SettingsViewControllerDelegate {
+    func setColor(color: UIColor) {
+        view.backgroundColor = color
+    }
+}
+
+
--- СolorizedApp/SettingsViewController.swift
+++ СolorizedApp/SettingsViewController.swift
@@ -8,57 +8,108 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    // MARK: - Outlets
     @IBOutlet var resultColorView: UIView!
     
-    @IBOutlet var redValueLabel: UILabel!
-    @IBOutlet var greenValueLabel: UILabel!
-    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
     
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    // MARK: - Public Propertis
+    var delegate: SettingsViewControllerDelegate!
+    var backgroundColor: UIColor!
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         resultColorView.layer.cornerRadius = 20
         
-        setColor()
-        setValue(for: redValueLabel, greenValueLabel, blueValueLabel)
+        getColor(from: backgroundColor)
+        createColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
+        setValueTF(for: redTextField, greenTextField, blueTextField)
     }
-
+    
+    //MARK: - IB Actions
     @IBAction func rgbSlider(_ sender: UISlider) {
-        setColor()
+        createColor()
         switch sender {
         case redSlider:
-            redValueLabel.text = string(from: redSlider)
+            redLabel.text = string(from: redSlider)
+            redTextField.text = string(from: redSlider)
         case greenSlider:
-            greenValueLabel.text = string(from: greenSlider)
+            greenLabel.text = string(from: greenSlider)
+            greenTextField.text = string(from: greenSlider)
         default:
-            blueValueLabel.text = string(from: blueSlider)
+            blueLabel.text = string(from: blueSlider)
+            blueTextField.text = string(from: blueSlider)
         }
     }
     
-    private func setColor() {
+    @IBAction func doneButtonPressed() {
+        dismiss(animated: true)
+        delegate.setColor(color: backgroundColor)
+    }
+  
+    // MARK: - Private Methods
+    private func createColor() {
         resultColorView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
         )
+        
+        backgroundColor = resultColorView.backgroundColor
     }
     
     private func setValue(for labels: UILabel...) {
         labels.forEach { label in
             switch label {
-            case redValueLabel:
-                redValueLabel.text = string(from: redSlider)
-            case greenValueLabel:
-                greenValueLabel.text = string(from: greenSlider)
+            case redLabel:
+                redLabel.text = string(from: redSlider)
+            case greenLabel:
+                greenLabel.text = string(from: greenSlider)
             default:
-                blueValueLabel.text = string(from: blueSlider)
+                blueLabel.text = string(from: blueSlider)
             }
         }
+    }
+    
+    private func setValueTF(for textFields: UITextField...) {
+        textFields.forEach { textField in
+            switch textField {
+            case redTextField:
+                redTextField.text = string(from: redSlider)
+            case greenTextField:
+                greenTextField.text = string(from: greenSlider)
+            default:
+                blueTextField.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func getColor(from color: UIColor) {
+        
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        redSlider.value = Float(red)
+        greenSlider.value = Float(green)
+        blueSlider.value = Float(blue)
     }
     
     private func string(from slider: UISlider) -> String {
